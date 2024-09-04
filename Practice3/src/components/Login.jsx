560aa561d3a21e2e7e727cf2841aafe8531e782c
Ramiro Trevisan <ramirotrevisan@gmail.com>
2024-09-04 18:40:50 -0300

    subo solucion

---
+++ Practice3/src/components/Login.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+
+    if (value.includes("o") || value.includes("O")) {
+      alert("Por favor, ¡Nombres de usuario sin la letra o!");
+      setUsername(""); // Limpiar el estado del nombre de usuario
+    } else {
+      setUsername(value); // Actualizar el estado si no contiene "o" u "O"
+    }
+  };
+
+  const handleRegister = () => {
+    if (username === "" || username.includes("o") || username.includes("O")) {
+      alert("Usuario inválido para registrarse");
+    } else {
+      alert("¡Usuario registrado correctamente!");
+    }
+  };
+
+  return (
+    <div>
+      <input 
+       
+        type="text"
+        value={username}
+        onChange={handleInputChange}
+        placeholder="Ingrese su nombre de usuario"
+      />
+      <br /> <br />
+      <button 
+      style={{ backgroundColor: "blue", color: "white" }}
+      onClick={handleRegister}>Registrarse</button>
+      <p>{username}</p>
+    </div>
+  );
+}
+
+export default Login;
